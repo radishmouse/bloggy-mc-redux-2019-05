4666bf06f6a68d96395d4dfe2cdf939640bfdbf9
chris aquino <chris@radishmouse.com>
2019-05-09 09:48:20 -0400

    adds posts reducer

---
+++ src/reducers/posts.js
@@ -0,0 +1,21 @@
+
+import {
+    ACTION_CREATE_POST
+} from '../actions';
+
+// A reducer is a function
+// that accepts the current state
+// and an action
+// then calculates/returns the next
+// new version of state
+export function posts(state={}, action={type: ''}) {
+    switch(action.type) {
+        case ACTION_CREATE_POST:
+            // create a post!
+            console.log(`Sounds like you wanna create a post `)
+        break;
+        default:
+            return state;
+        break;
+    }
+}
