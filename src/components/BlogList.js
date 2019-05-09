967f1b0987f3f1864b201182b0e6772dd4632971
chris aquino <chris@radishmouse.com>
2019-05-09 11:57:34 -0400

    updates BlogList so that it shows both the title and id

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 export default function BlogList({posts}) {
     // Helper function implicitly returns <li>
-    console.log(Object.values(posts));
-    const listItems = Object.values(posts).map(p => <li>{p.title}</li>);
+    // const listItems = Object.values(posts).map(p => <li>{p.title}</li>);
+    
+    // If we want the IDs as well as the blog posts,
+    // we should get an array of the IDs (using Object.keys).
+    // Then, we map over that array.
+    // With each id, we can access the blog post
+    // in the posts object.
+    const listItems = Object.keys(posts).map(id => {
+        const theBlogPost = posts[id];
+        return <li>{theBlogPost.title} - {id}</li>
+    });
     return (
         <ol>
             {listItems}
