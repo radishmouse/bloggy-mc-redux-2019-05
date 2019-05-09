bc7ba4eed9c8db7997c97e35f9a3bc1c9769c65d
chris aquino <chris@radishmouse.com>
2019-05-09 10:33:54 -0400

    adds posts in reducer with random id

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,6 +3,8 @@ import {
     ACTION_CREATE_POST
 } from '../actions';
 
+import { generateId } from '../utils';
+
 // A reducer is a function
 // that accepts the current state
 // and an action
@@ -12,7 +14,20 @@ export default function posts(state={}, action={type: ''}) {
     switch(action.type) {
         case ACTION_CREATE_POST:
             // create a post!
-            console.log(`Sounds like you wanna create a post `)
+            const id = generateId();
+            console.log(`the new id is ${id}`);
+            // console.log(`Sounds like you wanna create a post `);
+            const newState = {
+                ...state,
+                // To use a variable as a key
+                // in an object literal,
+                // wrap the variable in square brackets
+                [id]: action.payload
+            };
+            // To get the equivalent result, you
+            // can use the familar assignment syntax:
+            // newState[id] = action.payload;
+            return newState;
         break;
         default:
             return state;
