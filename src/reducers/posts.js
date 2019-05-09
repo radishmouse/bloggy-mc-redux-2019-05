79779daa779670522c00d027ad41a3db962c2e53
chris aquino <chris@radishmouse.com>
2019-05-09 11:00:41 -0400

    handles case where updating non existant post

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -40,13 +40,19 @@ export default function posts(state={}, action={type: ''}) {
         break;
         case ACTION_UPDATE_POST:
             // const {id, title, content} = action.payload;
-            return {
-                ...state,
-                [action.payload.id]: {
-                    title: action.payload.title || state[action.payload.id].title,
-                    content: action.payload.content || state[action.payload.id].content,
-                }
-            };
+            const postToUpdate = state[action.payload.id];
+            if (postToUpdate) {
+                return {
+                    ...state,
+                    [action.payload.id]: {
+                        title: action.payload.title || postToUpdate.title,
+                        content: action.payload.content || postToUpdate.content,
+                    }
+                };
+            } else {
+                // No post to update!
+                return state;
+            }
         break;
         default:
             return state;
